da6c77683b637dbef4b14ec7b4f0a500dfca06e6
ShirishkumarBari <shirishkumargbari@gmail.com>
2021-01-03 19:18:11 +0530

    adding internationalizationSupport

--- spring-rest-basic-with-JPA/src/main/java/com/example/spring/rest/SpringRestBasicWithJpaApplication.java
+++ spring-rest-basic-with-JPA/src/main/java/com/example/spring/rest/SpringRestBasicWithJpaApplication.java
@@ -2,12 +2,41 @@ package com.example.spring.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.MessageSourceResourceBundle;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringRestBasicWithJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestBasicWithJpaApplication.class, args);
+	}
+
+	/*@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSourceResourceBundle(){
+		ResourceBundleMessageSource messageSourceResourceBundle = new ResourceBundleMessageSource();
+		messageSourceResourceBundle.setBasename("messages");
+		return messageSourceResourceBundle;
+	}*/
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver sessionLocaleResolver = new AcceptHeaderLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
 	}
 
 }
--- spring-rest-basic-with-JPA/src/main/java/com/example/spring/rest/controller/HelloWorldController.java
+++ spring-rest-basic-with-JPA/src/main/java/com/example/spring/rest/controller/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.example.spring.rest.controller;
 
+import org.apache.tomcat.jni.Local;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
 
     @GetMapping("/hello-world")
     public String helloWorld(){
@@ -15,6 +25,16 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean/{message}")
     public HelloWorldBean helloWorldBean(@PathVariable String message){
         return new HelloWorldBean(message);
+    }
+
+    @GetMapping("/hello-world-i18")
+    public String helloWorldi18(@RequestHeader(required = false, name = "Accept-Language", defaultValue = "us") Locale locale){
+        return messageSource.getMessage("good.morning",null,locale);
+    }
+
+    @GetMapping("/hello-world-i18-generic")
+    public String helloWorldi18(){
+        return messageSource.getMessage("good.morning",null, LocaleContextHolder.getLocale());
     }
 
 }
